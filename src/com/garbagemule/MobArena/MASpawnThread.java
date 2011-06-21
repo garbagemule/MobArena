811daf958c0cd9c5fc6d65e27a82d450ae726c6c
Pneumatus <matt@g33k0r.org>
2011-06-21 19:05:32 +0100

    Highly experimental mob spawn distribution algorithm added to replace the standard even distribution.

--- src/com/garbagemule/MobArena/MASpawnThread.java
+++ src/com/garbagemule/MobArena/MASpawnThread.java
@@ -1,5 +1,7 @@
 package com.garbagemule.MobArena;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import org.bukkit.Location;
 import org.bukkit.entity.Wolf;
@@ -113,33 +115,36 @@ public class MASpawnThread implements Runnable
      */
     private void defaultWave()
     {
-        Location loc;
-        
-        for (int i = 0; i < wave + noOfPlayers; i++)
-        {
-            loc = ArenaManager.spawnpoints.get(i % noOfSpawnPoints);
-            ran = random.nextInt(dWolves);
-            CreatureType mob;
-            
-            /* Because of the nature of the if-elseif-else statement,
-             * we're able to evaluate the random number in this way.
-             * If dSpiders = 0, then dSpiders = dSkeletons, which
-             * means if the random number is below that value, we will
-             * spawn a skeleton and break out of the statement. */
-            if      (ran < dZombies)   mob = CreatureType.ZOMBIE;
-            else if (ran < dSkeletons) mob = CreatureType.SKELETON;
-            else if (ran < dSpiders)   mob = CreatureType.SPIDER;
-            else if (ran < dCreepers)  mob = CreatureType.CREEPER;
-            else if (ran < dWolves)    mob = CreatureType.WOLF;
-            else continue;
-            
-            LivingEntity e = ArenaManager.world.spawnCreature(loc,mob);
-            ArenaManager.monsterSet.add(e);
-            
-            // Grab a random target.
-            Creature c = (Creature) e;
-            c.setTarget(getClosestPlayer(e));
-        }
+    	Map<Location, Integer> weightedSpawnPoints = getWeightedSpawnPoints(wave + noOfPlayers);
+    	for (Map.Entry<Location, Integer> entry : weightedSpawnPoints.entrySet()) {
+	        Location loc = entry.getKey();
+	        int numToSpawn = entry.getValue();
+	        
+	        for (int i = 0; i < numToSpawn; i++)
+	        {
+	            ran = random.nextInt(dWolves);
+	            CreatureType mob;
+	            
+	            /* Because of the nature of the if-elseif-else statement,
+	             * we're able to evaluate the random number in this way.
+	             * If dSpiders = 0, then dSpiders = dSkeletons, which
+	             * means if the random number is below that value, we will
+	             * spawn a skeleton and break out of the statement. */
+	            if      (ran < dZombies)   mob = CreatureType.ZOMBIE;
+	            else if (ran < dSkeletons) mob = CreatureType.SKELETON;
+	            else if (ran < dSpiders)   mob = CreatureType.SPIDER;
+	            else if (ran < dCreepers)  mob = CreatureType.CREEPER;
+	            else if (ran < dWolves)    mob = CreatureType.WOLF;
+	            else continue;
+	            
+	            LivingEntity e = ArenaManager.world.spawnCreature(loc,mob);
+	            ArenaManager.monsterSet.add(e);
+	            
+	            // Grab a random target.
+	            Creature c = (Creature) e;
+	            c.setTarget(getClosestPlayer(e));
+	        }
+    	}
     }
     
     /**
@@ -147,7 +152,6 @@ public class MASpawnThread implements Runnable
      */
     private void specialWave()
     {
-        Location loc;
         CreatureType mob;
         ran = random.nextInt(dGhasts);
         
@@ -199,29 +203,33 @@ public class MASpawnThread implements Runnable
                 break;
         }
         
-        // Spawn the hippie monsters.
-        for (int i = 0; i < count; i++)
-        {
-            loc = ArenaManager.spawnpoints.get(i % noOfSpawnPoints);
-            
-            LivingEntity e = ArenaManager.world.spawnCreature(loc,mob);
-            if (!ArenaManager.monsterSet.contains(e))
-                ArenaManager.monsterSet.add(e);
-            else
-                System.out.println("MASpawnThread - monsterSet contains this entity");
-            
-            if (slime)   ((Slime)e).setSize(2);
-            if (wolf)    ((Wolf)e).setAngry(true);
-            if (ghast)   ((Ghast)e).setHealth(Math.min(noOfPlayers*25, 200));
-            if (creeper) ((Creeper)e).setPowered(true);
-            
-            // Slimes can't have targets, apparently.
-            if (!(e instanceof Creature))
-                continue;
-            
-            // Grab a random target.
-            Creature c = (Creature) e;
-            c.setTarget(getClosestPlayer(e));
+        Map<Location, Integer> weightedSpawnPoints = getWeightedSpawnPoints(count);
+        for (Map.Entry<Location, Integer> entry : weightedSpawnPoints.entrySet()) {
+        	Location loc = entry.getKey();
+        	int numToSpawn = entry.getValue();
+        	
+        	// Spawn the hippie monsters.
+	        for (int i = 0; i < numToSpawn; i++)
+	        {           
+	            LivingEntity e = ArenaManager.world.spawnCreature(loc,mob);
+	            if (!ArenaManager.monsterSet.contains(e))
+	                ArenaManager.monsterSet.add(e);
+	            else
+	                System.out.println("MASpawnThread - monsterSet contains this entity");
+	            
+	            if (slime)   ((Slime)e).setSize(2);
+	            if (wolf)    ((Wolf)e).setAngry(true);
+	            if (ghast)   ((Ghast)e).setHealth(Math.min(noOfPlayers*25, 200));
+	            if (creeper) ((Creeper)e).setPowered(true);
+	            
+	            // Slimes can't have targets, apparently.
+	            if (!(e instanceof Creature))
+	                continue;
+	            
+	            // Grab a random target.
+	            Creature c = (Creature) e;
+	            c.setTarget(getClosestPlayer(e));
+	        }
         }
         
         if (!ArenaManager.lightning)
@@ -275,5 +283,78 @@ public class MASpawnThread implements Runnable
         double d6 = loc.getZ() - d3;
         
         return d4*d4 + d5*d5 + d6*d6;
+    }
+    
+    /**
+     * Weight the list of spawn points 
+     * @param mobsToSpawn
+     * @return A map of locations with the number of mobs to spawn
+     */
+    private Map<Location, Integer> getWeightedSpawnPoints(int mobsToSpawn) {
+    	// Standard spawn algo is split evenly between spawn points.
+    	// This is more sexy, but also more expensive to calculate.
+    	
+    	int remainingMobsCount = mobsToSpawn;
+    	Map<Location, Integer> spawnTally = new HashMap<Location, Integer>();
+    	Map<Location, Integer> weightedSpawns = new HashMap<Location, Integer>();
+    	
+    	// Pre-populate spawnTally with all spawn points
+    	for (Location l : ArenaManager.spawnpoints) {
+    		spawnTally.put(l, 0);
+    	}
+    	
+    	// First, for each player in the arena, figure out which spawn point they are closest to
+    	for (Player player : ArenaManager.playerSet) {
+    		double nearestDistance = 0;
+    		Location nearestLocation = null;
+    		for (Location l : ArenaManager.spawnpoints) {
+    			double thisDistance = distance(l, player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
+    			if (null == nearestLocation || thisDistance < nearestDistance) {
+    				nearestLocation = l;
+    				nearestDistance = thisDistance;
+    			}
+    		}
+    		// All spawn points checked, add the nearest to the tally
+    		if (null != nearestLocation) {
+    			spawnTally.put(nearestLocation, spawnTally.get(nearestLocation) + 1);
+    		}
+    	}
+    	
+    	// Now we should have spawnTally indicating the number of closest players
+    	// there are to each spawn location. Build a weighted spawn list
+    	for (Map.Entry<Location, Integer> entry : spawnTally.entrySet()) {
+    		Location spawnLocation = entry.getKey();
+    		int nearestPlayers = entry.getValue();
+    		
+    		// Calculate the number of mobs to spawn at this location
+    		double ratio = (double) nearestPlayers / (double) ArenaManager.playerSet.size();
+    		int mobsForThisSpawn = (int) Math.abs(ratio * mobsToSpawn);
+    		weightedSpawns.put(spawnLocation, mobsForThisSpawn);
+    		remainingMobsCount -= mobsForThisSpawn;
+    	}
+    	
+    	// Now, due to rounding or players not being in range of spawn points, we might still have mobs to allocate
+    	// First attempt to distribute them around the already-recorded weighted spawn points list evenly
+    	if (!weightedSpawns.isEmpty()) {
+    		while (remainingMobsCount > 0) {
+    			for (Map.Entry<Location, Integer> entry : weightedSpawns.entrySet()) {
+    				weightedSpawns.put(entry.getKey(), weightedSpawns.get(entry.getKey()) + 1);
+    				remainingMobsCount--;
+    			}
+    		}
+    	}
+    	
+    	// If we had no weighted spawns, just throw down an even distribution
+    	// across all spawn points (a-la-vanilla algorithm);
+    	for (int i = remainingMobsCount; i > 0; i--) {
+    		Location l = ArenaManager.spawnpoints.get(i % noOfSpawnPoints);
+    		if (weightedSpawns.containsKey(l)) {
+    			weightedSpawns.put(l, weightedSpawns.get(l) + 1);
+    		} else {
+    			weightedSpawns.put(l, 1);
+    		}
+    	}
+    	
+    	return weightedSpawns;
     }
 }
