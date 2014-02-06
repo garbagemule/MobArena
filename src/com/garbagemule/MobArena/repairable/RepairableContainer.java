a527c4a842965b0375fa798337c2a895a0c70ce7
garbagemule <garbagemule@gmail.com>
2014-02-06 04:41:21 +0100

    Remove obsolete item storing for RepairableContainer.

--- src/com/garbagemule/MobArena/repairable/RepairableContainer.java
+++ src/com/garbagemule/MobArena/repairable/RepairableContainer.java
@@ -4,20 +4,18 @@ import org.bukkit.block.BlockState;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
-import com.garbagemule.MobArena.util.inventory.SerializableInventory;
+import org.bukkit.inventory.ItemStack;
 
 public class RepairableContainer extends RepairableBlock
 {
-    private SerializableInventory inv;
+    private ItemStack[] contents;
     
     public RepairableContainer(BlockState state, boolean clear) {
         super(state);
 
         // Grab the inventory of the block
         Inventory inv = ((InventoryHolder) state).getInventory();
-        
-        // Make a SerializableInventory
-        this.inv = new SerializableInventory(inv);
+        contents = inv.getContents();
         
         // Clear the inventory if prompted
         if (clear) inv.clear();
@@ -36,7 +34,7 @@ public class RepairableContainer extends RepairableBlock
         // Grab the inventory
         InventoryHolder cb = (InventoryHolder) getWorld().getBlockAt(getX(),getY(),getZ()).getState();
         Inventory chestInv = cb.getInventory();
-        
-        SerializableInventory.loadContents(chestInv, this.inv);
+
+        chestInv.setContents(contents);
     }
 }
