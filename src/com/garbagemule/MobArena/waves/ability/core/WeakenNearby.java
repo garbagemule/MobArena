cddfe15851b3aeb01e36c4c1a20908a1bac0d683
Sam D (TheComputerGeek2) <TheComputerGeek2@users.noreply.github.com>
2015-07-27 10:12:56 -0700

    Add Weaken Nearby ability

---
+++ src/com/garbagemule/MobArena/waves/ability/core/WeakenNearby.java
@@ -0,0 +1,33 @@
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import com.garbagemule.MobArena.framework.Arena;
+import com.garbagemule.MobArena.waves.MABoss;
+import com.garbagemule.MobArena.waves.ability.Ability;
+import com.garbagemule.MobArena.waves.ability.AbilityInfo;
+import com.garbagemule.MobArena.waves.ability.AbilityUtils;
+
+@AbilityInfo(name = "Weaken Nearby", aliases = { "weakennearby" })
+public class WeakenNearby implements Ability {
+
+	public static final int RADIUS = 5;
+	public static final int AMPLIFIER = 0;
+	public static final int DURATION = 60;
+
+	@Override
+	public void execute(Arena arena, MABoss boss) {
+
+		if (AbilityUtils.getTarget(arena, boss.getEntity(), false) == null) {
+			return;
+		}
+
+		for (Player e : AbilityUtils.getNearbyPlayers(arena, boss.getEntity(),
+				RADIUS)) {
+			e.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS,
+					DURATION, AMPLIFIER));
+		}
+
+	}
+
+}
