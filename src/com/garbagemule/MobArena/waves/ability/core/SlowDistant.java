86dbdee1e811d5d24cb4f71842f4fc0c8e98499e
Sam D (TheComputerGeek2) <TheComputerGeek2@users.noreply.github.com>
2015-07-27 10:20:44 -0700

    Add Slow Distant ability

---
+++ src/com/garbagemule/MobArena/waves/ability/core/SlowDistant.java
@@ -0,0 +1,28 @@
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import com.garbagemule.MobArena.framework.Arena;
+import com.garbagemule.MobArena.waves.MABoss;
+import com.garbagemule.MobArena.waves.ability.Ability;
+import com.garbagemule.MobArena.waves.ability.AbilityInfo;
+import com.garbagemule.MobArena.waves.ability.AbilityUtils;
+
+@AbilityInfo(name = "Slow Distant", aliases = { "slowdistant" })
+public class SlowDistant implements Ability {
+
+	public static final int RADIUS = 8;
+	public static final int DURATION = 60;
+	public static final int AMPLIFIER = 0;
+
+	@Override
+	public void execute(Arena arena, MABoss boss) {
+
+		for (Player e : AbilityUtils.getDistantPlayers(arena, boss.getEntity(),
+				RADIUS)) {
+			e.addPotionEffect(new PotionEffect(PotionEffectType.SLOW,
+					DURATION, AMPLIFIER));
+		}
+	}
+
+}
