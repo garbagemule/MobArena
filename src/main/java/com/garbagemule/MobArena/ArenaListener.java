3bdf11f3d3c84d7b3b2eed78bc48a9323a26514f
Haileykins <hailey.loralyn@gmail.com>
2018-05-02 07:25:11 +0000

    Fix Boss Kill Announcement Being Non-Customizable, Add localization Fixes #428

--- src/main/java/com/garbagemule/MobArena/ArenaListener.java
+++ src/main/java/com/garbagemule/MobArena/ArenaListener.java
@@ -607,12 +607,12 @@ public class ArenaListener
                 }
                 MABoss boss = monsters.getBoss(damagee);
                 if (boss != null) {
+                    for (Player q : arena.getPlayersInArena()) {
+                        arena.getMessenger().tell(q, Msg.WAVE_BOSS_KILLED, p.getName());
+                    }
                     Thing reward = boss.getReward();
                     if (reward != null) {
-                        String msg = p.getName() + " " + Msg.WAVE_BOSS_KILLED + " " + reward;
-                        for (Player q : arena.getPlayersInArena()) {
-                            arena.getMessenger().tell(q, msg);
-                        }
+                        arena.getMessenger().tell(damager, Msg.WAVE_BOSS_REWARD_EARNED, reward.toString());
                     }
                 }
             }
