335e3e81a6361ceb61547c8d739267ad1691e405
Andreas Troelsen <garbagemule@gmail.com>
2018-07-04 02:09:54 +0200

    Replace deprecated matcher in MoneyThingParserTest.

--- src/test/java/com/garbagemule/MobArena/things/MoneyThingParserTest.java
+++ src/test/java/com/garbagemule/MobArena/things/MoneyThingParserTest.java
@@ -4,7 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
