951e83cc55e30873d87a491cef5cdad6e9f4bf9c
Andreas Troelsen <garbagemule@gmail.com>
2018-08-06 01:22:27 +0200

    Rework some of the wave parsing logic.
    This commit is a full sweep across the WaveParser. It introduces the ConfigError exception to the WaveParser and throws it whenever something isn't right. There's quite a lot of stuff going on, but in overall:
    
    - Errors are thrown rather than null values being returned
    - Defaults are picked in lieu of explicit values when they "make sense"
    - Most list-type values can now be either the default comma-separated string or a string list
    
    Common nodes:
    - Spawnpoints can now be a string list
    
    Default waves:
    - Missing or empty monsters map throws an error
    - Missing wave growth defaults to medium
    - Invalid wave growth throws an error
    
    Special waves:
    - Missing or empty monsters map throws an error
    
    Swarm waves:
    - Missing or empty monster node throws an error
    - Missing swarm amount defaults to low
    - Invalid swarm amount throws an error
    
    Supply waves:
    - Missing or empty monsters map throws an error
    - Missing or empty drops node throws an error
    - Invalid drop throws an error
    - Drops can now be a string list
    
    Upgrade waves:
    - Missing or empty upgrades map throws an error
    
    Boss waves:
    - Missing or empty monster node throws an error
    - Missing or empty boss health defaults to medium
    - Invalid boss health throws an error
    - Invalid ability throws an error
    - Invalid drop throws an error
    - Abilities and drops can now be string lists

--- src/main/java/com/garbagemule/MobArena/waves/enums/SwarmAmount.java
+++ src/main/java/com/garbagemule/MobArena/waves/enums/SwarmAmount.java
@@ -1,7 +1,5 @@
 package com.garbagemule.MobArena.waves.enums;
 
-import com.garbagemule.MobArena.waves.WaveUtils;
-
 public enum SwarmAmount
 {
     LOW(10), MEDIUM(20), HIGH(30), PSYCHO(60);
@@ -13,9 +11,5 @@ public enum SwarmAmount
     
     public int getAmount(int playerCount) {
         return Math.max(1, playerCount / 2) * multiplier;
-    }
-    
-    public static SwarmAmount fromString(String string) {
-        return WaveUtils.getEnumFromString(SwarmAmount.class, string, LOW);
     }
 }
