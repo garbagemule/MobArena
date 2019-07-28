garbagemule/MobArena

commit 88ddbe5ac6ac5aa0f18ade50aa376b25546fb058

Lock food level for players in the lobby or spectator area.
There is no reason why food should be a thing in the lobby or spectator area. This commit effectively disables it for players in those places/states, while retaining the in-arena conditional logic based on the config-file setting.

Fixes #514

src/main/java/com/garbagemule/MobArena/ArenaListener.java
@@ -949,15 +949,22 @@ public class ArenaListener
     }
 
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
-        if (!arena.isRunning())
+        if (!(event.getEntity() instanceof Player)) {
             return;
+        }
 
-        if (!(event.getEntity() instanceof Player) || !arena.inArena((Player) event.getEntity()))
-            return;
+        Player p = (Player) event.getEntity();
 
-        // If the food level is locked, cancel all changes.
-        if (lockFoodLevel)
-            event.setCancelled(true);
+        if (arena.isRunning()) {
+            if (lockFoodLevel) {
+                event.setCancelled(true);
+            }
+        } else {
+            // Always locked in lobby/spec
+            if (arena.inLobby(p) || arena.inSpec(p)) {
+                event.setCancelled(true);
+            }
+        }
     }
 
     public void onPlayerAnimation(PlayerAnimationEvent event) {

commit ba41e51e48cc168dd8fc37f8db14db7b9fa16d65

Check if player is in arena on food level change.
This commit fixes a bug introduced in 88ddbe5ac6ac5aa0f18ade50aa376b25546fb058.

During a food level change event, if the arena is running and the food level lock is enabled, we just need to also make sure that the given player is actually in the arena, otherwise it affects everyone on the server.

Fixes #557

src/main/java/com/garbagemule/MobArena/ArenaListener.java
@@ -898,7 +898,7 @@ public class ArenaListener
         Player p = (Player) event.getEntity();
 
         if (arena.isRunning()) {
-            if (lockFoodLevel) {
+            if (arena.inArena(p) && lockFoodLevel) {
                 event.setCancelled(true);
             }
         } else {