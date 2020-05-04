58cb29ae975f343750f91936c4442e260f5cd7b0
Andreas Troelsen <garbagemule@gmail.com>
2020-05-04 07:59:57 +0200

    Remove obsolete class comment in Root Target.
    For some reason, this is the only ability that has such a comment, and
    it doesn't really provide any value, so yeet.

--- src/main/java/com/garbagemule/MobArena/waves/ability/core/RootTarget.java
+++ src/main/java/com/garbagemule/MobArena/waves/ability/core/RootTarget.java
@@ -10,23 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-/**
- * DISCLAIMER: The Ability source code is provided as-is, and the creator(s) of
- *             MobArena WILL NOT be held responsible for any damage that may
- *             result from altering the files.
- *              
- * WARNING:    Unless you know exactly what you are doing, i.e. you have a lot
- *             of experience with Java and Bukkit, you should never change any
- *             other values than those of the variables in CAPITAL LETTERS.
- *             
- *             
- * Root Target
- * Freezes the boss' target in place for ~3 seconds (default), by warping the
- * player to the same spot [ITERATIONS] times, with [TICKS] server ticks
- * between each iteration.
- * 
- * @author garbagemule
- */
 @AbilityInfo(
     name = "Root Target",
     aliases = {"roottarget", "freezetarget"}
