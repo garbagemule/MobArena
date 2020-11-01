06cedde031eeb50c0a915e5232be39854ca45b01
Andreas Troelsen <garbagemule@gmail.com>
2020-11-01 16:14:54 +0100

    Allow multi-word names in `/ma addarena` and `/ma autogenerate`.
    Introduces support for multi-word arena names in the two commands. The
    approach is to simply join the arguments by spaces. Because of the new
    slug-based lookups, multi-word names are fairly straightforward.

--- src/main/java/com/garbagemule/MobArena/commands/setup/AutoGenerateCommand.java
+++ src/main/java/com/garbagemule/MobArena/commands/setup/AutoGenerateCommand.java
@@ -7,6 +7,7 @@ import com.garbagemule.MobArena.commands.CommandInfo;
 import com.garbagemule.MobArena.commands.Commands;
 import com.garbagemule.MobArena.framework.Arena;
 import com.garbagemule.MobArena.framework.ArenaMaster;
+import com.garbagemule.MobArena.util.Slugs;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -27,24 +28,26 @@ public class AutoGenerateCommand implements Command
         }
 
         // Require an arena name
-        if (args.length != 1) return false;
+        if (args.length < 1) return false;
 
         // Unwrap the sender.
         Player p = Commands.unwrap(sender);
 
         // Check if arena already exists.
-        Arena arena = am.getArenaWithName(args[0]);
+        String name = String.join(" ", args);
+        String slug = Slugs.create(name);
+        Arena arena = am.getArenaWithName(slug);
         if (arena != null) {
             am.getGlobalMessenger().tell(sender, "An arena with that name already exists.");
             return true;
         }
 
-        if (!MAUtils.doooooItHippieMonster(p.getLocation(), 13, args[0], am.getPlugin())) {
+        if (!MAUtils.doooooItHippieMonster(p.getLocation(), 13, name, am.getPlugin())) {
             am.getGlobalMessenger().tell(sender, "Could not auto-generate arena.");
             return true;
         }
 
-        am.getGlobalMessenger().tell(sender, "Arena with name '" + args[0] + "' generated.");
+        am.getGlobalMessenger().tell(sender, "Arena with name '" + name + "' generated.");
         return true;
     }
 }
