bdf698dd4da77158505449bf00214810cf016c87
Maroon28 <74205260+Maroon28@users.noreply.github.com>
2021-08-07 15:48:55 +0200

    Add yet another forgotten `break;`

--- src/main/java/com/garbagemule/MobArena/waves/MACreature.java
+++ src/main/java/com/garbagemule/MobArena/waves/MACreature.java
@@ -154,6 +154,7 @@ public class MACreature {
                 break;
             case "hoglin":
                 ((Hoglin) e).setImmuneToZombification(true);
+                break;
             default:
                 break;
         }
